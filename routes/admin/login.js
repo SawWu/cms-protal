32b8dc3949ac20fae9a4a16850fc0bcff6aa48e1
Liu Ren <liuren@cc1st.com>
2018-11-18 16:22:49 +0800

    增加登录页面

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,7 +1,21 @@
 const router = require('koa-router')();
 
-router.get('/',async (ctx)=>{
+router.get('/', async (ctx) => {
   await ctx.render('views/admin/login');
+});
+
+router.post('/doLogin', async (ctx) => {
+
+  console.log(ctx.request.body);
+
+  let username = ctx.request.body.username;
+
+  let password = ctx.request.body.password;
+
+  let code = ctx.request.body.code;
+
+  ctx.redirect('/');
+
 })
 
-module.exports=router.routes();
+module.exports = router.routes();
